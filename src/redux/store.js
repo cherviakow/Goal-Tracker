833ddee5b47561d7ec72c75a828d188d908ded3e
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2025-05-09 10:52:40 -0400

    +

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const rootReducer = (state, action ) => {
+return state;
+}
+
+export const store = configureStore({
+    reducer: rootReducer,
+});
